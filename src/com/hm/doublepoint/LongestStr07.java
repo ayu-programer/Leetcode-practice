14be86eaeede88f3614cd93ca482c879c3686b51
Leetcode-practice <hanm188@163.com>
2020-11-18 10:35:48 +0800

    7. 最长子序列

--- src/com/hm/doublepoint/LongestStr07.java
+++ src/com/hm/doublepoint/LongestStr07.java
@@ -4,17 +4,16 @@
 * @author hanmeng
 * @date 2020-11-16  
 * @version 1.0 
-*/  
+*/
 package com.atguigu.algorithm.leetcode.arr;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 
 
 
-/**
+/**删除 s 中的一些字符，使得它构成字符串列表 d 中的一个字符串，找出能构成的最长字符串。如果有多个相同长度的结果，返回字典序的最小字符串。
  * @author hm
  *  Input:  s = "abpcplea", d = ["ale","apple","monkey","plea"]
  *	Output: "apple"
