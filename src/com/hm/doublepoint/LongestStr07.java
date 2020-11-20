2b7caeff3045ef49fe633e1d449c54e8b0e97a2c
Leetcode-practice <hanm188@163.com>
2020-11-20 13:52:35 +0800

    双指针问题：最长子序列

--- src/com/hm/doublepoint/LongestStr07.java
+++ src/com/hm/doublepoint/LongestStr07.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
 /**删除 s 中的一些字符，使得它构成字符串列表 d 中的一个字符串，找出能构成的最长字符串。如果有多个相同长度的结果，返回字典序的最小字符串。
- * @author hm
+ * @author hm 双指针问题
  *  Input:  s = "abpcplea", d = ["ale","apple","monkey","plea"]
  *	Output: "apple"
  */
